ad8e081398fd0146ac755c38148f0e7a7a209cc3
Fermin <46830086+Fermn@users.noreply.github.com>
2025-01-15 23:58:15 -0800

    fix class encapsulation - add gem requirements

--- app.rb
+++ app.rb
@@ -1,30 +1,43 @@
-get '/' do
-  erb :index
-end
-
-post '/convert' do
-  # Make sure file was uploaded
-  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-    return "No file uploaded"
+require 'sinatra'
+require 'axlsx'
+require 'ruby_powerpoint'
+require_relative 'lib/pptx_parser'
+class App < Sinatra::Base
+  get '/' do
+    erb :index
   end
 
-  # Parse PowerPoint file
-  pptx_data = PPTXParser.parse(tmpfile.path)
-
-  # Create Excel file
-  p = Axlsx::Package.new
-  wb = p.workbook
-  wb.add_worksheet(:name => "Data") do |sheet|
-    sheet.add_row[:Title, :Size_Card, :Color_Ways]
-    pptx_data.each do |row|
-      sheet.add_row row.values
+  post '/convert' do
+    # Make sure file was uploaded
+    unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+      return "No file uploaded"
     end
+
+    # Parse PowerPoint file
+    pptx_data = PPTXParser.parse(tmpfile.path)
+
+    # Create Excel file
+    p = Axlsx::Package.new
+    wb = p.workbook
+    wb.add_worksheet(:name => "Data") do |sheet|
+      sheet.add_row[:Title, :Size_Card, :Color_Ways]
+      pptx_data.each do |row|
+        sheet.add_row row.values
+      end
+    end
+
+    # Safe file to public folder
+    excel_filename = "output_#{Time.now.to_i}.xlsx"
+    p.serialize("public/#{excel_filename}")
+
+    # redirect to results page
+    redirect "/result?file=#{excel_filename}"
   end
 
-  # Safe file to public folder
-  excel_filename = "output_#{Time.now.to_i}.xlsx"
-  p.serialize("public/#{excel_filename}")
+  get '/result' do
+    @file = params[:file]
+    erb :result
+  end
 
-  # redirect to results page
-  redirect "/result?file=#{excel_filename}"
+  run! if app_file == $0
 end
